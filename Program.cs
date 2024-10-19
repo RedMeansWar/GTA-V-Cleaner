f8a05d60050d1fe6ed24b4965fc1ba48ca0cbd70
RedMeansWar <dev.brandon.o@gmail.com>
2024-10-19 08:25:25 -0500

    fixed array

--- Program.cs
+++ Program.cs
@@ -16,16 +16,17 @@ namespace GTA_V_Cleaner
         // List of all default GTA V files
         protected static readonly List<string> FILES_TO_SKIP = new()
         {
-            "bink2w64.dll", "common.rpf", "d3dcompiler_46.dll", "d3dcsx_46.dll", "GFSDK_ShadowLib.win64.dll", "GFSDK_TXAA.win64.dll", "GFSDK_TXAA_AlphaResolve.win64.dll",
-            "GPUPerfAPIDX11-x64.dll", "GTA5.exe", "GTAVLanguageSelect.exe", "GTAVLauncher.exe", "NvPmApi.Core.win64.dll", "GTA5_BE.exe", "steam_api64.dll", "version.txt",
+            "bink2w64.dll", "common.rpf", "d3dcompiler_46.dll", "d3dcsx_46.dll", "fvad.dll", "GFSDK_ShadowLib.win64.dll", "GFSDK_TXAA.win64.dll", "GFSDK_TXAA_AlphaResolve.win64",
+            "GPUPerfAPIDX11-x64.dll", "GTA5.exe", "GTA5_BE.exe", "GTAVLanguageSelect.exe", "GTAVLauncher.exe", "PlayGTAV.exe", "installscript.vdf", "installscript_sdk.vdf",
+            "libcurl.dll", "libtox.dll", "NvPmApi.Core.win64.dll", "opus.dll", "opusenc.dll", "steam_api64.dll", "title.rgl", "version.txt", 
             "x64a.rpf", "x64b.rpf", "x64c.rpf", "x64d.rpf", "x64e.rpf", "x64f.rpf", "x64g.rpf", "x64h.rpf", "x64i.rpf", "x64j.rpf", "x64k.rpf", "x64l.rpf", "x64m.rpf", "x64n.rpf",
-            "x64o.rpf", "x64p.rpf", "x64q.rpf", "x64r.rpf", "x64s.rpf", "x64t.rpf", "x64u.rpf", "x64v.rpf", "x64w.rpf", "installscript.vdf"
+            "x64o.rpf", "x64p.rpf", "x64q.rpf", "x64r.rpf", "x64s.rpf", "x64t.rpf", "x64u.rpf", "x64v.rpf", "x64w.rpf", "zlib1.dll"
         };
 
         // List of all default GTA V folders
         protected static readonly List<string> FOLDERS_TO_SKIP = new()
         {
-            "Installers", "Redistributables", "update", "x64", "modded", "BattlEye"
+            "Redistributables", "update", "x64", "Modded", "BattlEye"
         };
         #endregion
 
