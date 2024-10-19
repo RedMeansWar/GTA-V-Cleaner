1fec836135a2a0427f1cdbd0da092cf64fc3dad1
RedMeansWar <dev.brandon.o@gmail.com>
2024-10-19 08:02:18 -0500

    update Array List for GTA V BE

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace GTA_V_Cleaner
         protected static readonly List<string> FILES_TO_SKIP = new()
         {
             "bink2w64.dll", "common.rpf", "d3dcompiler_46.dll", "d3dcsx_46.dll", "GFSDK_ShadowLib.win64.dll", "GFSDK_TXAA.win64.dll", "GFSDK_TXAA_AlphaResolve.win64.dll",
-            "GPUPerfAPIDX11-x64.dll", "GTA5.exe", "GTAVLanguageSelect.exe", "GTAVLauncher.exe", "NvPmApi.Core.win64.dll", "PlayGTAV.exe", "steam_api64.dll", "version.txt",
+            "GPUPerfAPIDX11-x64.dll", "GTA5.exe", "GTAVLanguageSelect.exe", "GTAVLauncher.exe", "NvPmApi.Core.win64.dll", "GTA5_BE.exe", "steam_api64.dll", "version.txt",
             "x64a.rpf", "x64b.rpf", "x64c.rpf", "x64d.rpf", "x64e.rpf", "x64f.rpf", "x64g.rpf", "x64h.rpf", "x64i.rpf", "x64j.rpf", "x64k.rpf", "x64l.rpf", "x64m.rpf", "x64n.rpf",
             "x64o.rpf", "x64p.rpf", "x64q.rpf", "x64r.rpf", "x64s.rpf", "x64t.rpf", "x64u.rpf", "x64v.rpf", "x64w.rpf", "installscript.vdf"
         };
